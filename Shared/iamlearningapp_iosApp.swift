057f85ba5f5c0af31d893e816f4e84f5b7065eda
ahmet <ahmet@ahmet-MacBook-Pro.local>
2021-01-17 11:40:44 +0300

    First Commit

---
+++ Shared/iamlearningapp_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iamlearningapp_iosApp.swift
+//  Shared
+//
+//  Created by ahmet on 17.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct iamlearningapp_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
